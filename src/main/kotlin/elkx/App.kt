1f05b181b6c22fb1a417b84f106e9e39427725b0
adelnizamutdinov <stiggpwnz@gmail.com>
2021-12-09 11:10:59 +0300

    amen

---
+++ src/main/kotlin/elkx/App.kt
@@ -0,0 +1,50 @@
+package elkx
+
+import io.vertx.config.ConfigRetriever
+import io.vertx.core.http.HttpServer
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.coroutines.await
+
+private inline fun <T> ConfigRetriever.use(f: (ConfigRetriever) -> T): T {
+    try {
+        return f(this)
+    } finally {
+        close()
+    }
+}
+
+object ConfigKey {
+    const val PORT = "http.port"
+}
+
+object Routes {
+    const val JSON = "/json"
+}
+
+class App : CoroutineVerticle() {
+
+    private lateinit var server: HttpServer
+
+    override suspend fun start() {
+
+        val cfg = ConfigRetriever.create(vertx).use { it.config.await() }.mergeIn(config)
+        val port = cfg.getInteger(ConfigKey.PORT)
+
+        server = vertx.createHttpServer()
+            .requestHandler(Router.router(vertx).apply {
+                post(Routes.JSON)
+                    .handler(BodyHandler.create(false))
+                    .handler { ctx ->
+                        ctx.response().end()
+                    }
+            })
+            .listen(port, "127.0.0.1")
+            .await()
+    }
+
+    override suspend fun stop() {
+        server.close().await()
+    }
+}
---
+++ src/test/kotlin/elkx/AppTest.kt
@@ -0,0 +1,49 @@
+package elkx
+
+import io.vertx.core.DeploymentOptions
+import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.client.WebClient
+import io.vertx.kotlin.coroutines.await
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class AppTest {
+
+    private companion object {
+        val vertx = Vertx.vertx()!!
+
+        const val port = 8082
+
+        val deploy = vertx.deployVerticle(
+            App(),
+            DeploymentOptions().setConfig(JsonObject(mapOf(ConfigKey.PORT to port))),
+        )!!
+
+        val client = WebClient.create(vertx)!!
+
+        @JvmStatic
+        @BeforeAll
+        fun setUp(): Unit = runBlocking {
+            deploy.await()
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun tearDown(): Unit = runBlocking {
+            vertx.undeploy(deploy.await()).await()
+        }
+    }
+
+    @Test
+    fun hit(): Unit = runBlocking {
+        val r = client.post(port, "localhost", Routes.JSON)
+            .sendJsonObject(JsonObject())
+            .await()
+
+        assertEquals(expected = 200, actual = r.statusCode())
+    }
+}
