4b8ed3c1f43bb0874e1ced0cbdda618bf7559794
adelnizamutdinov <stiggpwnz@gmail.com>
2021-12-24 15:17:51 +0300

    real test

--- src/test/kotlin/elkx/AppTest.kt
+++ src/test/kotlin/elkx/AppTest.kt
@@ -6,13 +6,17 @@ import io.vertx.core.buffer.Buffer
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.client.WebClient
 import io.vertx.kotlin.coroutines.await
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.eclipse.elk.graph.json.JsonImporter
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import java.nio.file.NoSuchFileException
+import java.nio.file.Paths
+import kotlin.io.path.readText
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 class AppTest {
 
@@ -39,21 +43,25 @@ class AppTest {
         fun tearDown(): Unit = runBlocking {
             vertx.undeploy(deployment.await()).await()
         }
+
+        suspend fun resourceText(name: String): String =
+            withContext(Dispatchers.IO) {
+                val res = javaClass.classLoader.getResource(name) ?: throw NoSuchFileException(name)
+                Paths.get(res.toURI()).readText()
+            }
     }
 
     @Test
-    fun hit(): Unit = runBlocking {
+    fun realExample(): Unit = runBlocking {
         val r = client.post(port, "localhost", Routes.JSON)
-            .sendBuffer(Buffer.buffer(EX1))
+            .sendBuffer(Buffer.buffer(resourceText("serverless.json")))
             .await()
-
         assertEquals(expected = 200, actual = r.statusCode())
 
         val root = JsonImporter().transform(gsonParse(r.bodyAsString()))
-        val node = root.children.first()
-        assertTrue(node.x > 0)
-        assertTrue(node.y > 0)
-        assertTrue(node.width > 0)
-        assertTrue(node.height > 0)
+        assertEquals(expected = 339.0, actual = root.height)
+
+        val firstChild = root.children.first()
+        assertEquals(expected = 5.0, actual = firstChild.y)
     }
 }
